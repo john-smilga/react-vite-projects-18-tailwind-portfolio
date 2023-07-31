24aca063e8034874f3440a91a92a7b0baad0200a
john smilga <johnsmilga@gmail.com>
2023-07-31 13:29:06 -0400

    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,11 @@ const Hero = () => {
       <div className='mx-auto max-w-7xl  px-8 grid  md:grid-cols-2 h-full items-center gap-8'>
         <article>
           <h1 className='text-7xl font-bold tracking-wider'>I'm John</h1>
-          <p className='mt-4 text-2xl text-slate-700'>
-            Freelance Web And Mobile UI/UX Designer
+          <p className='mt-4 text-3xl text-slate-700 capitalize'>
+            Front-end developer
+          </p>
+          <p className='mt-2 text-lg text-slate-700 capitalize'>
+            turning ideas into interactive reality
           </p>
           <div className='flex gap-x-4 mt-4'>
             <a href='#'>
