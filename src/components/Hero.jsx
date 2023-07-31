a47380f08c19b8d701474cd7c4de3ebec20b37fb
john smilga <johnsmilga@gmail.com>
2023-07-31 13:40:08 -0400

    ready to record

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,8 @@ import heroImg from '../assets/hero.svg';
 import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa';
 const Hero = () => {
   return (
-    <section className='bg-emerald-100 h-[60vh] md:min-h-[60vh] '>
-      <div className='mx-auto max-w-7xl  px-8 grid  md:grid-cols-2 h-full items-center gap-8'>
+    <section className='bg-emerald-100 py-24  '>
+      <div className=' mx-auto max-w-7xl  px-8  grid  md:grid-cols-2 items-center gap-8'>
         <article>
           <h1 className='text-7xl font-bold tracking-wider'>I'm John</h1>
           <p className='mt-4 text-3xl text-slate-700 capitalize tracking-wide'>
@@ -25,8 +25,8 @@ const Hero = () => {
           </div>
         </article>
 
-        <article className='hidden md:block'>
-          <img src={heroImg} className='h-80' />
+        <article className='hidden md:block '>
+          <img src={heroImg} className='h-80 lg:h-96' />
         </article>
       </div>
     </section>
